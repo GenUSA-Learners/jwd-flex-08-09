c437f512372ee7d5606c2b4b0ec1211f36f70799
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2023-05-15 19:44:27 -0400

    fix temp literals unsolved

--- JS/01-intro-to-js/Activities/10-prac-template-literals/unsolved/script.js
+++ JS/01-intro-to-js/Activities/10-prac-template-literals/unsolved/script.js
@@ -9,7 +9,3 @@ let myStr5 = ".";
 // using the variables above print the following sentence to the console:
 // "The fox jumped over the fence."
 
-// template literal syntax
-console.log(`${myStr1} ${myStr2} ${myStr3} ${myStr4} ${myStr5}`);
-
-console.log(myStr1 +' '+ myStr2 + myStr3);
