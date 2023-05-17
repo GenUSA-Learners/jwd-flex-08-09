e4dbf96fac07671d5c26b72c8e766a72f25c2b42
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2023-05-17 10:14:14 -0400

    Reset unsolved activities

--- JS/01-intro-to-js/Activities/14-prac-ternary-operator/unsolved/script.js
+++ JS/01-intro-to-js/Activities/14-prac-ternary-operator/unsolved/script.js
@@ -8,8 +8,6 @@ if (isCorrect) {
   console.log('Incorrect!');
 }
 
-isCorrect ? console.log('Correct!') : console.log('Incorrect!');
-
 // Refactor the second if / else block to use a ternary operator
 let favoritePhrase = 'Love That!';
 
@@ -18,5 +16,3 @@ if (favoritePhrase === 'Love That!') {
 } else {
   console.log("I don't love that!");
 }
-
-favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log('I don`t love that');
