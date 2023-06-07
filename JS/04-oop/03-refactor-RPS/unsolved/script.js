42338db47c582c4853624dc6a3a2d0680604de1b
Jon Jackson <ocskier@gmail.com>
2023-06-07 18:34:06 -0400

    Fixes js unsolved file rps

--- JS/04-oop/03-refactor-RPS/unsolved/script.js
+++ JS/04-oop/03-refactor-RPS/unsolved/script.js
@@ -1,60 +1,60 @@
-function RPS() {
-  this.wins = 0;
-  this.losses = 0;
-  this.ties = 0;
-  this.getUserChoice = function () {
-    let userChoice = prompt(
-      "please choose 'r' for rock, 'p' for paper, or 's' for scissors"
-    );
-    return userChoice;
-  };
-  this.getComputerChoice = function () {
-    let randomNum = Math.floor(Math.random() * 3);
-    let computerChoice;
-    if (randomNum === 0) {
-      computerChoice = 'r';
-    } else if (randomNum === 1) {
-      computerChoice = 's';
-    } else if (randomNum === 2) {
-      computerChoice = 'p';
-    }
-    return computerChoice;
-  };
+// First hint
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
-  this.playGame = function () {
-    let userChoice = this.getUserChoice();
-    let computerChoice = this.getComputerChoice();
-    alert(`You chose: ${userChoice}\nComputer chose: ${computerChoice}`);
+const getUserChoice = function () {
+  let userChoice = prompt(
+    "please choose 'r' for rock, 'p' for paper, or 's' for scissors"
+  );
+  return userChoice;
+};
 
-    if (
-      (userChoice === 'r' && computerChoice === 's') ||
-      (userChoice === 's' && computerChoice === 'p') ||
-      (userChoice === 'p' && computerChoice === 'r')
-    ) {
-      this.wins++;
-      alert("You've won " + this.wins + ' time(s)!');
-    } else if (userChoice === computerChoice) {
-      this.ties++;
-      alert("You've tied " + this.ties + ' time(s).');
-    } else {
-      this.losses++;
-      alert("You've lost " + this.losses + ' time(s).');
-    }
+// Second hint
+const getComputerChoice = function () {
+  let randomNum = Math.floor(Math.random() * 3);
+  let computerChoice;
+  if (randomNum === 0) {
+    computerChoice = 'r';
+  } else if (randomNum === 1) {
+    computerChoice = 's';
+  } else if (randomNum === 2) {
+    computerChoice = 'p';
+  }
+  return computerChoice;
+};
 
-    //   4th hint
-    alert(
-      `Current Standings:\nWins: ${this.wins}\nLosses: ${this.losses}\nTies: ${this.ties}`
-    );
+// third hint
+const playGame = function () {
+  let userChoice = getUserChoice();
+  let computerChoice = getComputerChoice();
+  alert(`You chose: ${userChoice}\nComputer chose: ${computerChoice}`);
 
-    let playAgain = confirm('Do you want to play again?');
+  if (
+    (userChoice === 'r' && computerChoice === 's') ||
+    (userChoice === 's' && computerChoice === 'p') ||
+    (userChoice === 'p' && computerChoice === 'r')
+  ) {
+    wins++;
+    alert("You've won " + wins + ' time(s)!');
+  } else if (userChoice === computerChoice) {
+    ties++;
+    alert("You've tied " + ties + ' time(s).');
+  } else {
+    losses++;
+    alert("You've lost " + losses + ' time(s).');
+  }
 
-    let totalPlays = this.wins + this.losses + this.ties;
+  //   4th hint
+  alert(`Current Standings:\nWins: ${wins}\nLosses: ${losses}\n Ties: ${ties}`);
 
-    if (playAgain === true && totalPlays < 10) {
-      this.playGame();
-    }
-  };
-}
+  let playAgain = confirm('Do you want to play again?');
 
-const newGame = new RPS();
-newGame.playGame();
+  let totalPlays = wins + losses + ties;
+
+  if (playAgain === true && totalPlays < 10) {
+    playGame();
+  }
+};
+
+playGame();
